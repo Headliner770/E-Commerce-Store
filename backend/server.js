3a192882c1dea6a408b76664361fc60e4c5110d6
Headliner770 <paul248022@gmail.com>
2025-05-06 20:59:37 +0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 
-
 import authRoutes from "./routes/auth.route.js";
 
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
@@ -13,4 +13,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
+  connectDB();
 });
