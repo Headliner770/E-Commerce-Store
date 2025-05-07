768da55519218dbb51ae620f4ec1fb0f4cc52879
Headliner770 <paul248022@gmail.com>
2025-05-07 21:03:49 +0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -10,11 +12,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()) // allows...
+app.use(express.json()); // allows...
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
+
   connectDB();
 });
