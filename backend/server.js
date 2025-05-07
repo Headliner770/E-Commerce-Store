994966ee2b37c67fedbf7f768b1377ab23894b06
Headliner770 <paul248022@gmail.com>
2025-05-07 12:09:55 +0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,13 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 
 import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()) // allows...
 
 app.use("/api/auth", authRoutes);
 
