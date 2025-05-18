123fa2029cbd90e9f58ce7b3d6d89576102f9c98
Headliner770 <paul248022@gmail.com>
2025-05-18 21:12:36 +0300

    HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,12 @@
-import React from "react";
+const categories = [
+  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/tshirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
+  { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
+  { href: "/glasses", name: "Glasses", imageUrl: "/glasses.jpg" },
+  { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
+  { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
+  { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
+];
 
 const HomePage = () => {
   return <div>HomePage</div>;
