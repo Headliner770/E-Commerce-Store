52a4df5e103653d26efc9b8ea5ddca26e3a272be
Headliner770 <paul248022@gmail.com>
2025-05-21 19:00:27 +0300

    fix

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import axios from "../lib/axios";
 import { toast } from "react-hot-toast";
+// import { logout } from "../../../backend/controllers/auth.controller";
 
 export const useUserStore = create((set, get) => ({
   user: null,
@@ -17,7 +18,7 @@ export const useUserStore = create((set, get) => ({
 
     try {
       const res = await axios.post("/auth/signup", { name, email, password });
-      set({ user: res.data.user, loading: false });
+      set({ user: res.data, loading: false });
     } catch (error) {
       set({ loading: false });
       toast.error(error.response.data.message || "An error occurred");
@@ -29,10 +30,33 @@ export const useUserStore = create((set, get) => ({
 
     try {
       const res = await axios.post("/auth/login", { email, password });
-      set({ user: res.data.user, loading: false });
+      set({ user: res.data, loading: false });
     } catch (error) {
       set({ loading: false });
       toast.error(error.response.data.message || "An error occurred");
     }
   },
+
+  logout: async () => {
+    try {
+      await axios.post("/auth/logout");
+      set({ user: null });
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "An error occured during logout"
+      );
+    }
+  },
+
+  checkAuth: async () => {
+    set({ checkingAuth: true });
+    try {
+      const response = await axios.get("/auth/profile");
+      set({ user: response.data, checkingAuth: false });
+    } catch (error) {
+      set({ checkingAuth: false, user: null });
+    }
+  },
 }));
+
+// TODO  Implement the axios intercenptors for refreshing access tokes 15m
