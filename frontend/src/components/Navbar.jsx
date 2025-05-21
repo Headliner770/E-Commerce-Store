5c4272288f7adafbf1fcd633e16eeec937f279df
Headliner770 <paul248022@gmail.com>
2025-05-21 21:14:13 +0300

    AdminPage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,13 +34,20 @@ const Navbar = () => {
                   size={20}
                 />
                 <span className="hidden sm:inline">Cart</span>
-                <span className="absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out">
+                <span
+                  className="absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 text-xs
+                 group-hover:bg-emerald-400 transition duration-300 ease-in-out"
+                >
                   3
                 </span>
               </Link>
             )}
             {isAdmin && (
-              <Link className="bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium transition duration-300 ease-in-out flex items-center">
+              <Link
+                className="bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium transition
+                duration-300 ease-in-out flex items-center"
+                to={"/secret-dashboard"}
+              >
                 <Lock className="inline-block mr-1" size={18} />
                 <span className="hidden sm:inline">Dashboard</span>
               </Link>
